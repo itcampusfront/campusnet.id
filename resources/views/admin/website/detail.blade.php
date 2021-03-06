18badf20f11a5782ca838eff816a5d625529dca8
ajifatur <ajifatur2@gmail.com>
2021-03-06 13:15:39 +0700

    Update bug CRUD Website

---
+++ resources/views/admin/website/detail.blade.php
@@ -0,0 +1,13 @@
+@extends('template/admin/main')
+
+@section('title', 'Detail Website')
+
+@section('content')
+
+<div class="content">
+    <div class="bg-white rounded-3 shadow-sm py-3 px-4 mb-4">
+        <h5>Detail Website</h5>
+    </div>
+</div>
+  
+@endsection
