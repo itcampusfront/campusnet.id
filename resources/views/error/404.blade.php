1e77d7edb5dce389a2edc2c60482dd1df6fa80e0
Isna Prasetyo <praseetyaa@gmail.com>
2022-04-25 09:27:55 +0700

    bug fix

--- resources/views/error/404.blade.php
+++ resources/views/error/404.blade.php
@@ -18,10 +18,13 @@
       <h5 class="text-capitalize mb-4" data-aos="fade-up" data-aos-duration="2500">We are Sorry, but the page you are requested was <span style="color: rgba(0,0,0,.2);" class="fw-bold">not found</span></h5> 
       <a data-aos="fade-up" data-aos-duration="3000" href="/" class="btn btn-theme text-uppercase btn-theme-1 me-2">Home</a>
     </div>
-    <p class="position-absolute" style="top: 96%; transform: translate(-50%,-50%)!important; left: 50%!important;">Made with <i class="fa fa-heart" style="font-size: .8rem; color: rgba(230, 57, 70, 1)"></i></p>
+    <p class="position-absolute start-50 translate-middle" style="top: 93%">Made with <i class="fa fa-heart" style="font-size: .8rem; color: rgba(230, 57, 70, 1);"></i></p>
+		<p class="position-absolute start-50 translate-middle" style="top: 96%"><i class="fab fa-github"></i> Powered by <a href="https://spandiv.xyz/" class="text-white" target="_blank">Spandiv</a></p>
   </div>
 </div>
 <style type="text/css">
+  .start-50{left:50%}
+  .translate-middle {transform: translate(-50%,-50%)!important;}
   nav {display: none!important;}
   footer{display: none;}
   h1{font-size: 12rem}
